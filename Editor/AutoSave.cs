ff0d66918ef1708d67f691e211c9857d2f5e5382
UserIsDevious <user.devious@gmail.com>
2020-10-17 02:22:21 +0900

    test

---
+++ Editor/AutoSave.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class AutoSave : EditorWindow
+{
+    int StorageInterval = 0;
+    string fileName = "FileName";
+    bool on;
+
+
+    [MenuItem("Tools/AutoSave")]
+    static void Create()
+    {
+        // 生成
+        GetWindow<AutoSave>("設定");
+    }
+    [MenuItem("Tools/Test")]
+    void ExampleOnGUI()
+    {
+        EditorGUILayout.ToggleLeft("save prefab", on);
+    }
+    public void OnGUI()
+    {
+        
+        EditorGUILayout.ToggleLeft("save prefab", on);
+        fileName = EditorGUILayout.TextField("File Name:", fileName);
+        //　hpシリアライズデータを表示
+        EditorGUILayout.IntSlider(StorageInterval, 0, 100);
+
+        if (GUILayout.Button("Save"))
+        {
+            seve();
+        }
+    }
+    void seve()
+    {
+        bool check = EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
+        Debug.Log("AutoSave" + (check ? "OK" : "Error"));
+    }
+    static AutoSave()
+    {
+        EditorApplication.playModeStateChanged += LogPlayModeState;
+    }
+    private static void LogPlayModeState(PlayModeStateChange state)
+    {
+        Debug.Log(state);
+    }
+}
